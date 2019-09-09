de7ef573c42cac439bd2bf36263f4f23d781c967
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-09 20:35:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  sentence = ""
+  array.each do |word|
+    if word == array.last
+      sentence << "and #{word}"
+    else
+      sentence << "#{word}, "
+    end
+  end
+  return sentence
 end
